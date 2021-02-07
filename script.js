0de0bca4dcd2c290d679dbbb78a037093291dfa8
nutchanonc <edu.nutchanon@gmail.com>
2021-02-08 02:20:30 +0700

    Updated

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const myMessage = [];
 
 let darkmode = false;
 
+const font_black = "rgb(31,31,31)"
+
 function GET() {
     fetch("https://backend.cpsk-club.xyz/twitter" , {
         method: "GET",
@@ -70,7 +72,13 @@ function scrollDown() {
     app.scrollTop = app.scrollHeight;
 }
 
+
+
+
+
 function toggleDarkMode() {
+
+    
     author = document.getElementsByClassName('author');
     for (var i=0 ; i<author.length ; i++) {
         author[i].style.color = 'white';
@@ -78,7 +86,7 @@ function toggleDarkMode() {
 
     chatlayout = document.getElementsByClassName('chat-layout');
     for (var i=0 ; i<chatlayout.length ; i++) {
-        chatlayout[i].style.boxShadow = '0 4px 10px black';
+        chatlayout[i].style.boxShadow = '0 4px 10px rgb(20, 20, 20)';
     }
 
     me = document.getElementsByClassName('me');
@@ -91,19 +99,73 @@ function toggleDarkMode() {
         my[i].style.boxShadow = '0 4px 10px black';
     }
 
-
+    document.getElementById("title").style.color = 'white';
     document.getElementsByTagName("BODY")[0].style.backgroundColor = 'rgb(24,24,24)';
     document.getElementById('app').style.backgroundColor           = 'rgb(30,30,30)';
     document.getElementById('app').style.boxShadow                 = '0 0 0 black';
     document.getElementById('footer').style.backgroundColor        = 'black';
     document.getElementById('label-author').style.color            = 'white';
     document.getElementById('label-content').style.color           = 'white';
-    
+    document.getElementById('toggle').innerHTML = 'l';
+    document.getElementById('toggle').style.backgroundColor        = 'rgb(52, 235, 146)';
+    document.getElementById('little-icon').src = 'pics/moon-icon-23634.png';
+    document.getElementById('description').style.color = 'white';
+    document.getElementById('main-logo').style.filter = 'invert(1)';
+   
 }
+
+
+
+
+
 function toggleLightMode() {
+    author = document.getElementsByClassName('author');
+    for (var i=0 ; i<author.length ; i++) {
+        author[i].style.color = font_black;
+    }
 
+    chatlayout = document.getElementsByClassName('chat-layout');
+    for (var i=0 ; i<chatlayout.length ; i++) {
+        chatlayout[i].style.boxShadow = ' 0 8px 10px rgb(175, 175, 175)';
+    }
+
+    me = document.getElementsByClassName('me');
+    for (var i=0 ; i<me.length ; i++) {
+        me[i].style.color = font_black;
+    }
+
+    my = document.getElementsByClassName('my');
+    for (var i=0 ; i<my.length ; i++) {
+        my[i].style.boxShadow = '0 8px 10px rgb(175, 175, 175)';
+    }
+
+    document.getElementById("title").style.color = 'black';
+    document.getElementsByTagName("BODY")[0].style.backgroundColor = 'white';
+    document.getElementById('app').style.backgroundColor           = 'rgb(255, 255, 255';
+    document.getElementById('app').style.boxShadow                 = 'rgb(180, 180, 180)';
+    document.getElementById('footer').style.backgroundColor        = 'rgb(34, 34, 34)';
+    document.getElementById('label-author').style.color            = font_black;
+    document.getElementById('label-content').style.color           = font_black;
+    document.getElementById('toggle').innerHTML = 'O';
+    document.getElementById('toggle').style.backgroundColor        = 'white';
+    document.getElementById('little-icon').src = 'pics/sun.png';
+    document.getElementById('description').style.color = 'black';
+    document.getElementById('main-logo').style.filter = 'invert(0)';
 }
 
+
+function toggleDisplayMode() {
+    if (darkmode) {
+        toggleLightMode();
+        darkmode = false;
+        console.log(darkmode)
+    }
+    else {
+        toggleDarkMode();
+        darkmode = true;
+        console.log(darkmode)
+    }
+}
 
 setInterval(() => {
     GET()
